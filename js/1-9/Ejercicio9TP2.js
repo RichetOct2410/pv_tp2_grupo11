fb1a10a60d6ff7bdd84b9f7194b2aaf1a7fb4cd4
Victor Efimov <victor-efimov>
2026-04-29 19:44:20 -0300

    Depuracion de codigo y creacion de ciertos archivos faltantes[VictorEfimov]

---
+++ js/1-9/Ejercicio9TP2.js
@@ -0,0 +1,5 @@
+const APELLIDO = prompt(`Por favor, ingrese su apellido:`);
+        const NOMBRE = prompt(`Ingrese su nombre:`);
+        alert(`¡Hola ${NOMBRE} ${APELLIDO}. 
+        Bienvenido a Programación Visual!`);
+    
