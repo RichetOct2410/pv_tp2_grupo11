4a6c209c20c059c0c179dd5c9a97c05b5aa3be5d
NicolasAzamor <ncealqez@gmail.com>
2026-04-30 22:35:01 -0300

    agregue una import al punto 20

--- src/js/19-25/Ejercicio20TP2.js
+++ src/js/19-25/Ejercicio20TP2.js
@@ -1,4 +1,5 @@
 import { servicesvictorefimov } from "../../../services/servicesvictorefimov.js";
 import {servicesOctavioRicci} from "../../../services/servicesOctavioRicci.js";
+import {servicesAzamorNicolas} from "../../../services/servicesOctavioRicci.js";
 servicesvictorefimov();
 servicesOctavioRicci();
